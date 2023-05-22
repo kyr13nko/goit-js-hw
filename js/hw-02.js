21cbfa30a5fc912a99ca735fb597991cbde32fb3
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-05-22 23:34:38 +0300

    x.x

--- js/hw-02.js
+++ js/hw-02.js
@@ -229,7 +229,7 @@ console.log(calculateTotalPrice([412, 371, 94, 63, 176])); */
 
 // ++++++++++++++ ТЕСТ 21 ++++++++++++++
 
-function findLongestWord(string) {
+/* function findLongestWord(string) {
   // Розділити рядок на окремі слова за допомогою пробілу як роздільника
   const words = string.split(" ");
 
@@ -237,7 +237,7 @@ function findLongestWord(string) {
   let longestWord = "";
 
   // Пройтися по кожному слову і знайти найдовше
-  for (let i = 0; i < words.length; i++) {
+  for (let i = 0; i < words.length; i += 1) {
     const currentWord = words[i];
 
     // Порівняти довжину поточного слова з найдовшим словом
@@ -252,12 +252,71 @@ function findLongestWord(string) {
 
 console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 console.log(findLongestWord("Google do a roll"));
-console.log(findLongestWord("May the force be with you"));
+console.log(findLongestWord("May the force be with you")); */
 
 // ++++++++++++++ ТЕСТ 22 ++++++++++++++
+
+/* function createArrayOfNumbers(min, max) {
+  const numbers = [];
+  for (let i = min; i <= max; i += 1) {
+    numbers.push(i);
+  }
+  return numbers;
+}
+
+console.log(createArrayOfNumbers(1, 3));
+console.log(createArrayOfNumbers(14, 17));
+console.log(createArrayOfNumbers(29, 34)); */
+
 // ++++++++++++++ ТЕСТ 23 ++++++++++++++
+
+/* function filterArray(numbers, value) {
+  let newArray = [];
+  for (let i = 0; i <= numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      newArray.push(numbers[i]);
+    }
+  }
+  return newArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20)); */
+
 // ++++++++++++++ ТЕСТ 24 ++++++++++++++
+
+/* function checkFruit(fruit) {
+  const fruits = ["apple", "plum", "pear", "orange"];
+
+  return fruits.includes(fruit);
+}
+console.log(checkFruit("plum"));
+console.log(checkFruit("mandarin"));
+console.log(checkFruit("pear"));
+console.log(checkFruit("Pear"));
+console.log(checkFruit("apple")); */
+
 // ++++++++++++++ ТЕСТ 25 ++++++++++++++
+
+/* function getCommonElements(array1, array2) {
+  const newArray = [];
+  for (let i = 0; i < array1.length; i += 1) {
+    if (array2.includes(array1[i])) {
+      newArray.push(array1[i]);
+    }
+  }
+  return newArray;
+}
+
+console.log(getCommonElements([1, 2, 3], [2, 4]));
+console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+console.log(getCommonElements([1, 2, 3], [10, 20, 30])); */
+
 // ++++++++++++++ ТЕСТ 26 ++++++++++++++
 // ++++++++++++++ ТЕСТ 27 ++++++++++++++
 // ++++++++++++++ ТЕСТ 28 ++++++++++++++

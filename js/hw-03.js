2a97c13acdef050c1d79e94aca7278463b105071
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-05-29 23:38:25 +0300

    x.x

--- js/hw-03.js
+++ js/hw-03.js
@@ -375,10 +375,107 @@ console.log(calculateTotalPrice("Scanner")); */
 
 // ++++++++++++++ ТЕСТ 21 ++++++++++++++
 
+/* const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+
+const { yesterday, today, tomorrow } = highTemperatures;
+
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+console.log(meanTemperature); */
+
 // ++++++++++++++ ТЕСТ 22 ++++++++++++++
+
+/* const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+
+const {
+  yesterday,
+  today,
+  tomorrow,
+  icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+} = highTemperatures;
+
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+console.log(meanTemperature);
+console.log(icon); */
+
 // ++++++++++++++ ТЕСТ 23 ++++++++++++++
+
+/* const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+
+const {
+  yesterday: highYesterday,
+  today: highToday,
+  tomorrow: highTomorrow,
+  icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+} = highTemperatures;
+
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+console.log(meanTemperature); */
+
 // ++++++++++++++ ТЕСТ 24 ++++++++++++++
+
+/* const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+const hexColors = [];
+const rgbColors = [];
+
+for (const { hex, rgb } of colors) {
+  hexColors.push(hex);
+  rgbColors.push(rgb);
+}
+
+console.log(hexColors);
+console.log(rgbColors); */
+
 // ++++++++++++++ ТЕСТ 25 ++++++++++++++
+
+/* const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+
+const {
+  today: {
+    low: lowToday,
+    high: highToday,
+    icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+  },
+  tomorrow: {
+    low: lowTomorrow,
+    high: highTomorrow,
+    icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+  },
+} = forecast;
+
+console.log(todayIcon);
+console.log(tomorrowIcon); */
+
 // ++++++++++++++ ТЕСТ 26 ++++++++++++++
 // ++++++++++++++ ТЕСТ 27 ++++++++++++++
 // ++++++++++++++ ТЕСТ 28 ++++++++++++++
